bed78f57edc77338701ccbe51c8074411b715cf1
Mike Murray <MikeMurrayDev@gmail.com>
2019-09-27 15:48:08 -0700

    adds updated app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+      priority: '',
+      editEnabled: false,
+      deleteItem: false
+    };
+    
+  }
 
   render() {
     return (
